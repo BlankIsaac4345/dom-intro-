f9a29cd4496225b62b74b52ca4564b3e02f7d2f5
BlankIsaac4345 <isaacml4345@gmail.com>
2026-03-02 18:38:17 +0000

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Script started");
+
+// variable to track number of clicks
+let clicks = 0;
+
+// variable to store the clci dispaly h1 element
+let clickDisplay = document.getElementById("click-display");
+
+// set the initial text for clicks count h1
+clickDisplay.innerText = "Clicks: " + clicks;
+
+// called each time the image is clicked 
+function handleClick() {
+    console.log("click");
+
+    // add one to click count
+    clicks = clicks + 1;
+
+    // update display
+    clickDisplay.innerText = "Clicks: " + clicks;
+}
